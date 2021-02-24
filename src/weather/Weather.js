dde416bc2bca0f5d699a6691993bc729fa5ef7e5
Gera184 <oogeraoo159@gmail.com>
2021-02-24 16:25:47 +0200

    first commit

--- src/weather/Weather.js
+++ src/weather/Weather.js
@@ -10,7 +10,7 @@ export const Weather = () => {
   var [query, setQuery] = useState("");
   var [weather, setWeather] = useState([]);
 
-  var URL = `http://api.openweathermap.org/data/2.5/weather?q=${query},&appid=${API_KEY}`;
+  var URL = `https://api.openweathermap.org/data/2.5/weather?q=${query},&appid=${API_KEY}`;
 
   function data() {
     axios
